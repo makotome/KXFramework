b46fa035fe27453f46e6d4a4e55eadf12eec27f5
潘圣能 <panshengneng@icloud.com>
2017-10-15 02:17:35 +0800

    KEngine.Component.Json	解析Json的类库

--- Assets/Plugins/KEngine/KEngine.Modules.Json/SimpleJson/SimpleJson.cs
+++ Assets/Plugins/KEngine/KEngine.Modules.Json/SimpleJson/SimpleJson.cs
@@ -1,10 +1,29 @@
-﻿// VERSION:
+//-----------------------------------------------------------------------
+// <copyright file="SimpleJson.cs" company="The Outercurve Foundation">
+//    Copyright (c) 2011, The Outercurve Foundation.
+//
+//    Licensed under the MIT License (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//      http://www.opensource.org/licenses/mit-license.php
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+// <author>Nathan Totten (ntotten.com), Jim Zimmerman (jimzimmerman.com) and Prabir Shrestha (prabir.me)</author>
+// <website>https://github.com/facebook-csharp-sdk/simple-json</website>
+//-----------------------------------------------------------------------
+
+// VERSION:
 
 // NOTE: uncomment the following line to make SimpleJson class internal.
-//#define SIMPLE_JSON_INTERNAL
+#define SIMPLE_JSON_INTERNAL
 
 // NOTE: uncomment the following line to make JsonArray and JsonObject class internal.
-//#define SIMPLE_JSON_OBJARRAYINTERNAL
+#define SIMPLE_JSON_OBJARRAYINTERNAL
 
 // NOTE: uncomment the following line to enable dynamic support.
 //#define SIMPLE_JSON_DYNAMIC
@@ -17,7 +36,7 @@
 
 // NOTE: uncomment the following line to disable linq expressions/compiled lambda (better performance) instead of method.invoke().
 // define if you are using .net framework <= 3.0 or < WP7.5
-//#define SIMPLE_JSON_NO_LINQ_EXPRESSION
+#define SIMPLE_JSON_NO_LINQ_EXPRESSION
 
 // NOTE: uncomment the following line if you are compiling under Window Metro style application/library.
 // usually already defined in properties
@@ -498,6 +517,7 @@ namespace SimpleJson
 
         private static readonly char[] EscapeTable;
         private static readonly char[] EscapeCharacters = new char[] { '"', '\\', '\b', '\f', '\n', '\r', '\t' };
+        //private static readonly string EscapeCharactersString = new string(EscapeCharacters);
 
         static SimpleJson()
         {
--- README.md
+++ README.md
@@ -22,3 +22,29 @@ UnityEditorVersion  2017.1.1p2
 
 
 在使用过程中遇到或者发现错误，欢迎pull 和issues，共同维护KSFramework。
+
+### KEngine包括：
+序号|模块|作用|是否必选|备注 |
+----|-----|--------|--------|--------|
+1| KEngine|核心基础 |是|v0.0.1|
+2| KEngine.Editor|核心基础编辑器 |是|v0.0.1|
+3| KEngine.Editor.Tools|核心基础编辑器工具 |是|v0.0.1|
+4| KEngine.Lib|核心库 |是|v0.0.1|
+5| KEngine.Lib.Network|网络库|是|v0.0.1|
+6| KEngine.Lib.UI|UI|是|v0.0.1|
+7| KEngine.Lib.UI.Editor|UI库编辑器|是|v0.0.1|
+8| KEngine.Component.Events|事件系统|否|v0.0.1|
+9| KEngine.Component.Debugger|为调试控制器组件|否|v0.0.1|
+10| KEngine.Component.Json|解析Json的类库|否|v0.0.1|
+11| KEngine.Component.Translation|国际化翻译组件|否|v0.0.1|
+12| KEngine.Component.Converters|转换器组件|否|v0.0.1|
+13| KEngine.Component.Random|随机数库|否|v0.0.1|
+14| KEngine.Component.Hashing|哈希库|否|v0.0.1|
+15| KEngine.Component.Encryption|加密库|否|v0.0.1|
+16| KEngine.Component.Compress|压缩解压缩库|否|v0.0.1|
+17| KEngine.Component.Timer|计时器 |否|v0.0.1|
+18| KEngine.Tools|工具集合|是|v0.0.1|
+
+
+
+
